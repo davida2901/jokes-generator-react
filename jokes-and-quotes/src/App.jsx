606436633ff644328f01c10ab99631d24980009e
davida2901 <davidadon29@gmail.com>
2023-04-24 00:57:33 -0400

    Added a navBar with router

--- jokes-and-quotes/src/App.jsx
+++ jokes-and-quotes/src/App.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import DadJokes from './DadJokes'
-import DadJokesWithAPi from './DadJokesWithAPi'
+import React from "react";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import DadJokes from "./DadJokes";
+import DadJokesWithAPi from "./DadJokesWithAPi";
+import "./styles/jokes.css";
 
 function App() {
   return (
-    <>
-    <DadJokes/>
-    <DadJokesWithAPi/>
-    </>
-  )
+    <BrowserRouter>
+      <div>
+        <nav className="navbar">
+          <ul>
+            <li className="item">
+              <Link className="item-text" to="/">Home</Link>
+            </li>
+            <li className="item">
+              <Link className="item-text" to="/jokes">Jokes</Link>
+            </li>
+            <li className="item">
+              <Link className="item-text" to="/jokes-with-api">Jokes From API</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<h1>Test</h1>} exact />
+          <Route path="/jokes" element={<DadJokes />} exact />
+          <Route path="/jokes-with-api" element={<DadJokesWithAPi />} exact />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
